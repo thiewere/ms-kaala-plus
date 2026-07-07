bec8bdd4dc39b1fa6527257244eac2d3182836a3
ousmane sow <thifuge@gmail.com>
2026-07-07 23:10:43 +0200

    feat(clients/service): add CRUD methodes

--- src/main/java/com/thifuge/kaala_plus/clients/ClientRepository.java
+++ src/main/java/com/thifuge/kaala_plus/clients/ClientRepository.java
@@ -2,5 +2,5 @@ package com.thifuge.kaala_plus.clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 }
