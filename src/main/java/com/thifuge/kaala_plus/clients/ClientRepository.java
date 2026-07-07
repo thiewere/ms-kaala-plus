14c288f30b94a7cae496367e89274f7050f60420
ousmane sow <thifuge@gmail.com>
2026-07-07 22:46:20 +0200

    feat(clients): add client repository

---
+++ src/main/java/com/thifuge/kaala_plus/clients/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.thifuge.kaala_plus.clients;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
