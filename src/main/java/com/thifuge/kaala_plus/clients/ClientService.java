04a896f9b343a8c721698b4a77532f3b5bcb6fcd
ousmane sow <thifuge@gmail.com>
2026-07-07 23:10:43 +0200

    feat(clients/service): add CRUD methodes

--- src/main/java/com/thifuge/kaala_plus/clients/ClientService.java
+++ src/main/java/com/thifuge/kaala_plus/clients/ClientService.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -15,4 +17,33 @@ public class ClientService {
         log.info("Creating a new client: {}", client);
         clientRepository.save(client);
     }
+
+    public List<Client> findAllClients() {
+        log.info("Finding all clients");
+        return clientRepository.findAll();
+    }
+
+    public Client findClientById(int id) {
+        log.info("Finding client by id: {}", id);
+        return clientRepository.findById(id).get();
+    }
+
+    public Client updateClient(int id, Client client) {
+        log.info("Updating client by id: {}", client);
+        Client clientInDb = this.findClientById(id);
+        clientInDb.setFirstname(client.getFirstname());
+        clientInDb.setLastname(client.getLastname());
+        clientInDb.setPhone(client.getPhone());
+        clientInDb.setCountry(client.getCountry());
+        clientInDb.setCreatedAt(client.getCreatedAt());
+        clientInDb.setUpdatedAt(client.getUpdatedAt());
+        clientInDb = clientRepository.save(clientInDb);
+        return clientInDb;
+    }
+
+    public void deleteClient(int id) {
+        log.info("Deleting client by id: {}", id);
+        clientRepository.deleteById(id);
+    }
+
 }
