5ffe88e7566ad641fc275bcc34a32cdb96f6d43e
ousmane sow <thifuge@gmail.com>
2026-07-09 00:59:14 +0200

    feat(expenses/service): implement RUD methodes

--- src/main/java/com/thifuge/kaala_plus/expenses/ExpenseService.java
+++ src/main/java/com/thifuge/kaala_plus/expenses/ExpenseService.java
@@ -2,9 +2,13 @@ package com.thifuge.kaala_plus.expenses;
 
 import com.thifuge.kaala_plus.orders.Order;
 import com.thifuge.kaala_plus.orders.OrderService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Slf4j
@@ -19,5 +23,37 @@ public class ExpenseService {
         Order orderInDB = this.orderService.findOrderByReference(order.getReference());
         expense.setOrder(orderInDB);
         this.expenseRepository.save(expense);
+    }
+
+    public List<Expense> findAllExpenses() {
+        return this.expenseRepository.findAll();
+    }
+
+    public Expense findExpense(Integer id) {
+        Optional<Expense> expense = this.expenseRepository.findById(id);
+        return expense.orElseThrow(() -> new EntityNotFoundException(
+                "Expense with id " + id + " not found"
+        ));
+    }
+
+    public Expense updateExpense(Integer id, Expense expense) {
+        Expense expenseInDB = this.findExpense(id);
+
+        log.info("Updating expense with id " + id + " from " + expenseInDB);
+        expenseInDB.setExpenseType(expense.getExpenseType());
+        expenseInDB.setOrder(expense.getOrder());
+        expenseInDB.setDescription(expense.getDescription());
+        expenseInDB.setExpenseDate(expense.getExpenseDate());
+        expenseInDB.setAmount(expense.getAmount());
+        expenseInDB.setUpdatedAt(expense.getUpdatedAt());
+
+        expenseInDB = this.expenseRepository.save(expenseInDB);
+        return expenseInDB;
+    }
+
+    public void deleteExpense(Integer id) {
+        Expense expenseInDB = this.findExpense(id);
+        log.info("Deleting expense with id " + id);
+        this.expenseRepository.delete(expenseInDB);
     }
 }
