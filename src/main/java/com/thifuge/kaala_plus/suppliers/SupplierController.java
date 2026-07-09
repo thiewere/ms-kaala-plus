27d5e330d6896ec8d8db89bc062ab0be8031ee8a
ousmane sow <thifuge@gmail.com>
2026-07-09 15:18:49 +0200

    feat(suppliers/controller): implement CRUP APIs

---
+++ src/main/java/com/thifuge/kaala_plus/suppliers/SupplierController.java
@@ -0,0 +1,39 @@
+package com.thifuge.kaala_plus.suppliers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("suppliers")
+public class SupplierController {
+
+    private SupplierService supplierService;
+
+    @PostMapping
+    public void create(@RequestBody Supplier supplier) {
+        this.supplierService.createSupplier(supplier);
+    }
+
+    @GetMapping
+    public List<Supplier> findAll() {
+        return this.supplierService.findAllSuppliers();
+    }
+
+    @GetMapping(path = "{id}")
+    public Supplier findSupplier(@PathVariable int id) {
+        return this.supplierService.findSupplier(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public Supplier updateSupplier(@PathVariable int id, @RequestBody Supplier supplier) {
+        return this.supplierService.updateSupplier(id, supplier);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteSupplier(@PathVariable int id) {
+        this.supplierService.deleteSupplier(id);
+    }
+}
