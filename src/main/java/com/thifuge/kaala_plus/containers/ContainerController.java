f712e6d3b25acea5547b87b30c8f71017ce7431d
ousmane sow <thifuge@gmail.com>
2026-07-11 02:46:44 +0200

    feat(containers/controller): implement CRUD APIs

---
+++ src/main/java/com/thifuge/kaala_plus/containers/ContainerController.java
@@ -0,0 +1,54 @@
+package com.thifuge.kaala_plus.containers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("containers")
+public class ContainerController {
+
+    private final ContainerService containerService;
+
+    @PostMapping
+    public void create(@RequestBody Container container) {
+        this.containerService.createContainer(container);
+    }
+
+    @GetMapping
+    public List<Container> findAllContainers() {
+        return this.containerService.findAllContainers();
+    }
+
+    @GetMapping(path = "{id}")
+    public Container findContainer(@PathVariable Integer id) {
+        return this.containerService.findContainer(id);
+    }
+
+    @GetMapping(path = "{reference}")
+    public Container findContainer(@PathVariable String reference) {
+        return this.containerService.findContainer(reference);
+    }
+
+    @PutMapping(path = "{id}")
+    public Container updateContainer(@PathVariable Integer id, @RequestBody Container container) {
+        return this.containerService.updateContainer(id, container);
+    }
+
+    @PutMapping(path = "{reference}")
+    public Container updateContainer(@PathVariable String reference, @RequestBody Container container) {
+        return this.containerService.updateContainer(reference, container);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteContainer(@PathVariable Integer id) {
+        this.containerService.deleteContainer(id);
+    }
+
+    @DeleteMapping(path = "{reference}")
+    public void deleteContainer(@PathVariable String reference) {
+        this.containerService.deleteContainer(reference);
+    }
+}
