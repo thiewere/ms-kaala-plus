43326029c41da88ac56a01f67671df259470430f
ousmane sow <thifuge@gmail.com>
2026-07-11 02:15:03 +0200

    feat(payments/service): implement CRUD APIs

--- src/main/java/com/thifuge/kaala_plus/payments/PaymentController.java
+++ src/main/java/com/thifuge/kaala_plus/payments/PaymentController.java
@@ -1,4 +1,39 @@
 package com.thifuge.kaala_plus.payments;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("payments")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public void  createPayment(@RequestBody Payment payment) {
+        this.paymentService.createPayment(payment);
+    }
+
+    @GetMapping
+    public List<Payment> findAllPayments() {
+        return this.paymentService.findAllPayments();
+    }
+
+    @GetMapping(path = "{id}")
+    public Payment findPayment(@PathVariable Integer id) {
+        return this.paymentService.findPayment(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public Payment updatePayment(@PathVariable Integer id, @RequestBody Payment payment) {
+        return this.paymentService.updatePayment(id, payment);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePayment(@PathVariable Integer id) {
+        this.paymentService.deletePayment(id);
+    }
 }
