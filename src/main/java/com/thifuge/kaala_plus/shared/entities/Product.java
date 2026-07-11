2fe09275467a2854c2e42f7fd9b63ce7e8e74b5d
ousmane sow <thifuge@gmail.com>
2026-07-11 04:57:06 +0200

    feat(shared/products): generate entity from db

---
+++ src/main/java/com/thifuge/kaala_plus/shared/entities/Product.java
@@ -0,0 +1,21 @@
+package com.thifuge.kaala_plus.shared.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 40)
+    private String name;
+
+
+}
