7fc76c83e70c5c7383099a52ed8989770ecf62cd
ousmane sow <thifuge@gmail.com>
2026-07-31 00:33:55 +0200

    feat(supp-prod): add SupÃplierProduct repository

---
+++ src/main/java/com/thifuge/kaala_plus/supplier_products/SupplierProductRepository.java
@@ -0,0 +1,6 @@
+package com.thifuge.kaala_plus.supplier_products;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierProductRepository extends JpaRepository<SupplierProduct, Integer> {
+}
