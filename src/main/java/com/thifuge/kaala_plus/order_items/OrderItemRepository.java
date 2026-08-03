edb685c10eaa64f747711aba2ea59b1f412e4df0
ousmane sow <thifuge@gmail.com>
2026-08-03 16:32:34 +0200

    feat(ord-items): add entity repository

---
+++ src/main/java/com/thifuge/kaala_plus/order_items/OrderItemRepository.java
@@ -0,0 +1,6 @@
+package com.thifuge.kaala_plus.order_items;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+}
