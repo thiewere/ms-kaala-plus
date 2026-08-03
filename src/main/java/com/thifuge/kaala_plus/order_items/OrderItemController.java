d5e9de95c54e9967dad5e0a73eb5c62fd929ad27
ousmane sow <thifuge@gmail.com>
2026-08-03 17:21:09 +0200

    feat(ord-items/controller): implement CRUD APIs

--- src/main/java/com/thifuge/kaala_plus/order_items/OrderItemController.java
+++ src/main/java/com/thifuge/kaala_plus/order_items/OrderItemController.java
@@ -1,4 +1,40 @@
 package com.thifuge.kaala_plus.order_items;
 
+import com.thifuge.kaala_plus.orders.Order;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("order_items")
 public class OrderItemController {
+
+    private final OrderItemService orderItemService;
+
+    @PostMapping()
+    public void createOrderItem(@RequestBody OrderItem orderItem) {
+        this.orderItemService.createOrderItem(orderItem);
+    }
+
+    @GetMapping()
+    public List<OrderItem> findAllOrderItems() {
+        return this.orderItemService.findAllOrderItems();
+    }
+
+    @GetMapping(path = "{id}")
+    public OrderItem findOrderItem(@PathVariable int id) {
+        return this.orderItemService.findOrderItem(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public OrderItem updateOrderItem(@RequestBody OrderItem orderItem, @PathVariable int id) {
+        return this.orderItemService.updateOrderItem(id, orderItem);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteOrderItem(@PathVariable int id) {
+        this.orderItemService.deleteOrderItem(id);
+    }
 }
