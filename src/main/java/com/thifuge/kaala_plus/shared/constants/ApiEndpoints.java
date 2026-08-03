450e6829e515ec66329bdb695b0fbaec0ceccdd5
ousmane sow <thifuge@gmail.com>
2026-08-03 19:04:31 +0200

    feat(shared/endpoints): implement general APIs endpoints

---
+++ src/main/java/com/thifuge/kaala_plus/shared/constants/ApiEndpoints.java
@@ -0,0 +1,18 @@
+package com.thifuge.kaala_plus.shared.constants;
+
+public final class ApiEndpoints {
+
+    public static final String API = "/api";
+
+    public static final String CLIENT = API + "/clients";
+    public static final String CONTAINER = API + "/containers";
+    public static final String EXPENSES = API + "/expenses";
+    public static final String ORDER_ITEMS = API + "/order_items";
+    public static final String ORDERS = API + "/orders";
+    public static final String PAYMENTS = API + "/payments";
+    public static final String PRODUCTS = API + "/products";
+    public static final String SUPPLIER_PRODUCTS = API + "/supplier_products";
+    public static final String SUPPLIERS = API + "/suppliers";
+
+    private ApiEndpoints() {}
+}
